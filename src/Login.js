5722325cf59f5f3ce5e5e336542eb98d911f4060
SOUHAILMENSI <souhaiel.mainsi@gmail.com>
2021-06-02 01:37:59 +0100

    final update

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { setUserSession } from "./Utils/Common";
 import "./resources/login.css";
 
+import Logo from "./imgs/ic_launcher.png";
+
 function Login(props) {
   const [loading, setLoading] = useState(false);
   const username = useFormInput("");
@@ -37,25 +39,36 @@ function Login(props) {
         <div className="col-lg-10 col-xl-9 mx-auto">
           <div className="card card-login flex-row my-5">
             <div className="card-img-left d-none d-md-flex"></div>
-            <div class="card-body">
-              
-              <h5 className="card-title text-center ">Netperf Analytics</h5>
+            <div className="card-body">
+              <img
+                alt=""
+                src={Logo}
+                width="50"
+                height="50"
+                className="rounded mx-auto d-block"
+              />
+              {""}
+              <h5 className="card-title text-center text-justify text-uppercase">
+                Netperf Analytics
+              </h5>
               <form className="form-login">
                 <div className="form-label-group">
                   <input
-                    type="text" {...username}
+                    type="text"
+                    {...username}
                     id="inputUserame"
                     className="form-control"
                     placeholder="Username"
                     required
                     autofocus
                   ></input>
-                  <label for="inputUserame" >Username</label>
+                  <label for="inputUserame">Username</label>
                 </div>
 
                 <div class="form-label-group">
                   <input
-                    type="password" {...password}
+                    type="password"
+                    {...password}
                     id="inputPassword"
                     className="form-control"
                     placeholder="Password"
@@ -64,11 +77,23 @@ function Login(props) {
                   <label for="inputPassword">Password</label>
                 </div>
 
-                {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+                {error && (
+                  <>
+                    <div class="alert alert-danger" role="alert">
+                      <small style={{ color: "red" }}>{error}</small>
+                      <br />
+                    </div>
+                  </>
+                )}
+
+                <br />
                 <hr />
                 <button
                   className="btn btn-lg btn-primary btn-block text-uppercase"
-                  type="submit" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading}
+                  type="submit"
+                  value={loading ? "Loading..." : "Login"}
+                  onClick={handleLogin}
+                  disabled={loading}
                 >
                   Login
                 </button>
